19693ee0924f81d25b61d1c647e2ec44ebb764fd
tuolomee <kejmarova@gmail.com>
2021-05-01 12:39:56 +0200

    zprovoznění navigace

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navButtonElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('nav');
+
+navButtonElm.addEventListener('click', () => {
+  const navElm = document.querySelector('nav');
+  navElm.classList.toggle('nav-closed');
+});
+
+const navElmAll = document.querySelectorAll('nav a');
+
+for (let i = 0; i < navElmAll.length; i += 1) {
+navElmAll[i].addEventListener('click', () => {
+  navElm.classList.add('nav-closed');
+});
+}
+
+// navElmAll.forEach((item) => {
+//   item.addEventListener('click', () => {
+//     navElm.classList.add('nav-closed');
+//   });
+// });
