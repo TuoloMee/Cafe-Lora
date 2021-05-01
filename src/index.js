8091f93f1292382995dedaf53d39e3f9d1b03b35
tuolomee <kejmarova@gmail.com>
2021-05-01 13:35:39 +0200

    objednávání nápoje

--- src/index.js
+++ src/index.js
@@ -23,3 +23,21 @@ navElmAll[i].addEventListener('click', () => {
 //     navElm.classList.add('nav-closed');
 //   });
 // });
+// Na tlačíko se třídou order-btn pověste posluchač, při kliknutí změní nápis na tlačítku na Zrušit. Zároveň přidá na prvek se třídou drink__cup třídu drink__cup--selected.
+// Při opětovném kliknutí na tlačítko chceme zařídit, aby se objednávka zrušila a změny na prvku .drink__cup i na tlačítku .order-btn se vrátily (odebrala se třída drink__cup--selected a vrátil se text tlačítka na Objednat). Tohoto můžete docílit například tak, že si vytvoříte globální proměnnou ordered, která bude obsahovat true nebo false podle toho, zde je nápoj objednaný či nikoliv.
+
+const orderBtn = document.querySelector('.order-btn');
+const drinkCup = document.querySelector('.drink__cup');
+let ordered = true;
+
+orderBtn.addEventListener('click', () => {
+  if (ordered === true) {
+  orderBtn.textContent = 'Zrušit';
+  drinkCup.classList.add('drink__cup--selected');
+  ordered = false;
+} else {
+  orderBtn.textContent = 'Objednat';
+  drinkCup.classList.remove('drink__cup--selected');
+  ordered = true
+}
+});
